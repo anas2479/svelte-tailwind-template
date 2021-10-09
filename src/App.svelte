edaab17eae31b242084051e6a8d8eb3c880f4d44
Anas Mohamed <amohamed2479@gmail.com>
2021-10-09 07:04:05 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<div class="max-w-[600px] mx-auto mt-10">
+<h1 class="text-7xl font-medium text-gray-700 text-center">Svelte + Tailwind</h1>
+</div>
+
+
+<script>
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode:'jit',
+  purge: [
+    './src/**/*.svelte',
+    './src/**/*.js',
+    './src/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
